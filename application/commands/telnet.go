bc2aaddfb0ade44441c70b10ffbe56a580d9946d
itay oren <119848771+itayoren2005@users.noreply.github.com>
2026-05-21 18:48:04 +0300

    Fix telnet conflict approval hang while waiting for takeover

--- application/commands/telnet.go
+++ application/commands/telnet.go
@@ -157,10 +157,12 @@ func (d *telnetClient) acquireOrConfirm(buf []byte) bool {
 
 		telnetSessions.disconnectOthers(d.remoteIP, d)
 
-		if !telnetSessions.conflicts(d.remoteIP, d) {
-			d.acquired = true
-			return true
+		for telnetSessions.conflicts(d.remoteIP, d) {
+			time.Sleep(10 * time.Millisecond)
 		}
+
+		d.acquired = true
+		return true
 	}
 }
 
@@ -301,6 +303,27 @@ func (d *telnetClient) client(
 }
 
 func (d *telnetClient) Close() error {
+	d.stateLock.Lock()
+	acquired := d.acquired
+	remoteConn := d.remoteConn
+	d.stateLock.Unlock()
+
+	if !acquired {
+		select {
+		case d.decision <- false:
+		default:
+		}
+
+		d.closeWait.Wait()
+		return nil
+	}
+
+	if remoteConn != nil {
+		remoteConn.Close()
+		d.closeWait.Wait()
+		return nil
+	}
+
 	remoteConn, remoteConnErr := d.getRemote()
 
 	if remoteConnErr == nil {
